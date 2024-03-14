97c208f2efce1037044078058aaefaef7b6a6275
Lucas David <lucascanellas@id.uff.br>
2024-03-14 19:21:03 -0300

    reset

--- src/main/java/com/tcc/gestaoclinica/domain/repositories/GroupMedicalCareRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tcc.gestaoclinica.domain.repositories;
-
-import com.tcc.gestaoclinica.domain.models.GroupMedicalCare;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupMedicalCareRepository extends JpaRepository<GroupMedicalCare, Long>{
-}
